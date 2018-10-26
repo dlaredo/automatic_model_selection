c09a73c1e8cc5487f11f01f1862335c20b556ed3
dlaredo <davidlaredo1@gmail.com>
2018-10-25 18:37:28 -0700

    Genotype (list) mapped to Fenotype (architecture)

--- code/nn_encoding_list.py
+++ code/nn_encoding_list.py
@@ -3,21 +3,23 @@ from anytree.exporter import DotExporter
 import random
 
 import keras
+import keras.layers
 from keras.models import Sequential, Model
-from keras.layers import Dense, Input, Dropout, Reshape, Conv2D, Flatten, MaxPooling2D, LSTM
-from keras.optimizers import Adam, SGD
-from keras.callbacks import LearningRateScheduler
-from keras import backend as K
-from keras import regularizers
+#from keras.layers import Dense, Input, Dropout, Reshape, Conv2D, Flatten, MaxPooling2D, LSTM
+#from keras.optimizers import Adam, SGD
+#from keras.callbacks import LearningRateScheduler
+#from keras import backend as K
+#from keras import regularizers
 
 """Layer types
 
 	FC:1
 	Conv:2
-	Pooling:3
-	RNN:4
-	PerturbateParam:5
-	Empty:6
+	RNN:3
+	Pooling:4
+	Dropout:5
+	PerturbateParam:6
+	Empty:7
 
 """
 
@@ -29,20 +31,26 @@ from keras import regularizers
 	3: Filters for cnn
 	4: Kernel_size for cnn (just one for squared kernels)
 	5: Strides for cnn
+	6: Pooling size
+	7: Dropout rate
 
 """
 
 ann_building_rules = {
 	
-					1:[1],
-					2:[1, 2, 3],
-					3:[1, 2],
-					4:[1, 4],
-					5:[],
-					6:[1, 2, 3, 4]
+					1:[1, 5],
+					2:[1, 2, 4, 5],
+					3:[1, 3, 5],
+					4:[1, 2],
+					5:[1, 2, 3],
+					6:[],
+					7:[1, 2, 3, 4]
 }
 
-def generate_model(model=None, prev_component=6, next_component=6, max_layers=64, more_layers_prob=0.8):
+activations = {0:'sigmoid', 1:'tanh', 2:'relu', 3:'softmax', 4:'linear'}
+
+
+def generate_model(model=None, prev_component=7, next_component=7, max_layers=64, more_layers_prob=0.8):
 	"""Iteratively and randomly generate a model"""
 
 	layer_count = 0
@@ -51,13 +59,16 @@ def generate_model(model=None, prev_component=6, next_component=6, max_layers=64
 	if model == None:
 		model = list()
 
-
 	while True:
 
 		curr_component = random.choice(ann_building_rules[prev_component])
 
-		if curr_component == 5:
-			ann_building_rules[5] = ann_building_rules[prev_component]
+		#Perturbate param layer
+		if curr_component == 6:
+			ann_building_rules[6] = ann_building_rules[prev_component]
+		elif curr_component == 5: #Dropout layer
+			ann_building_rules[5] = ann_building_rules[prev_component].copy()
+			ann_building_rules[5].remove(5)
 
 		rndm = random.random()
 		#print(rndm)
@@ -67,7 +78,7 @@ def generate_model(model=None, prev_component=6, next_component=6, max_layers=64
 		#Keep adding more layers
 		if more_layers == False:
 			#Is this layer good for ending?
-			if next_component == 6 or next_component in ann_building_rules[curr_component]:
+			if next_component == 7 or next_component in ann_building_rules[curr_component]:
 				layer = generate_layer(curr_component)
 				model.append(layer)
 				prev_component = curr_component
@@ -92,44 +103,154 @@ def generate_model(model=None, prev_component=6, next_component=6, max_layers=64
 def generate_layer(layer_type):
 	"""Given a layer type, return the layer params"""
 
-	layer = [layer_type, 0, 0, 0, 0, 0]
+	layer = [layer_type, 0, 0, 0, 0, 0, 0, 0]
 	layer[1] = 8*random.randint(1,129) #Generate a random number of neurons which is a multiple of 8 up to 1024 neurons
 	layer[2] = random.randint(0,3) if layer_type != 5 else 2
 	layer[3] = 8*random.randint(1,64)
 	layer[4] = 3**random.randint(1,6)
 	layer[5] = random.randint(1,6)
+	layer[6] = 2**random.randint(1,6)
+	layer[7] = random.uniform(0, 0.5)
 
 	return layer
 
-
-	"""
-	if layer_type == 2: #FC layer
-	elif layer_type == 3: #Conv layer
-	elif layer_type == 4: #Pooling layer
-	elif layer_type == 5: #RNN layer
-	else:
-	"""
-
-def decode_genotype(model_genotype):
+def decode_genotype(model_genotype, problem_type, input_shape, output_dim):
 	"""From a model genotype, generate the keras model"""
 
 	model = Sequential()
+	return_sequences = False
 
-	print(model_genotype)
+	#print(model_genotype)
 
-	for i in range(len(model_genotype)):
+	#Fetch first layer
+	curr_layer = model_genotype[0]
+	curr_layer_type = curr_layer[0]
+	next_layer_type = model_genotype[1][0]
 
-		print(i)
-		layer = model_genotype[i]
-		print(layer)
+	#print("\n\n\n")
+	#print(curr_layer)
+
+	if next_layer_type == 3:  #If the next layer is LSTM return sequences is true
+		return_sequences = True
+
+	klayer = array_to_layer(curr_layer, problem_type=problem_type, input_shape=input_shape, first_layer=True, return_sequences=return_sequences)
+	#print(klayer)
+
+	if klayer != None:
+		model.add(klayer)
+	else:
+		print("Model could not be fetched")
+		return None
+
+	if next_layer_type == 1:
+		if curr_layer_type == 2: #If the next layer is FC and current is Conv, Flatten output
+			model.add(keras.layers.Flatten())
+
+	#Fetch layers 1,...,l-1
+	for i in range(1,len(model_genotype)):
+
+		return_sequences = False
+
+		curr_layer = model_genotype[i]
+		curr_layer_type = curr_layer[0]
+		next_layer_type = model_genotype[i][0]
+
+		#print(curr_layer)
+
+		if next_layer_type == 3:  #If the next layer is LSTM return sequences is true
+			return_sequences = True
+
+		klayer = array_to_layer(curr_layer, return_sequences=return_sequences)
+		#print(klayer)
+
+		if klayer != None:
+			model.add(klayer)
+		else:
+			print("Model could not be fetched")
+			return None
+
+		if next_layer_type == 1:
+			if curr_layer_type == 2: #If the next layer is FC and current is Conv, Flatten output
+				model.add(keras.layers.Flatten())
+
+	#Fetch last layer
+	curr_layer = model_genotype[-1]
+	#print(curr_layer)
+	klayer = array_to_layer(curr_layer, problem_type=problem_type, output_dim=output_dim, last_layer=True)
+	#print(klayer)
+
+	if klayer != None:
+		model.add(klayer)
+	else:
+		print("Model could not be fetched")
+		return None
+
+	model.add(klayer)
+
+	return model
 
 
 
+def array_to_layer(array, problem_type=0, input_shape=(0,), output_dim=0, first_layer=False, last_layer=False, return_sequences=False):
+	"""Map from an array to a layer"""
 
-"""Input can be of 3 types, ANN (1), CNN (2) or RNN (4)"""
+	klayer = None
+	neurons_units = array[1]
+	activation = array[2]
+	filter_size = array[3]
+	kernel_size = array[4]
+	stride = array[5]
+	pool_size = array[6]
+	dropout_rate = array[7]
+
+	if first_layer == True:
+
+		if array[0] == 1:
+			klayer = keras.layers.Dense(neurons_units, input_shape=input_shape, activation=activations[activation], kernel_initializer='glorot_normal', name='in')
+		elif array[0] == 2:
+			klayer = keras.layers.LSTM(neurons_units, input_shape=input_shape, activation=activations[activation], kernel_initializer='glorot_normal', 
+				return_sequences=return_sequences, name='in')
+		elif array[0] == 3:
+			klayer = keras.layers.Conv2D(filter_size, (kernel_size, kernel_size), strides=(stride, stride), input_shape=input_shape, padding='valid',
+				activation=activations[activation], kernel_initializer='glorot_normal', name='in')
+		else:
+			print("Layer not valid for the first layer")
+			klayer = None
+
+	elif last_layer == True:
+
+		if problem_type == 1: #Classification
+			klayer = keras.layers.Dense(1, activation='linear', kernel_initializer='glorot_normal', name='out')
+		elif problem_type == 2: #Regression
+			klayer = keras.layers.Dense(output_dim, activation='softmax', kernel_initializer='glorot_normal', name='out')
+		else:
+			print("Layer not valid for the last layer")
+			klayer = None
+	else:
+
+		if array[0] == 1:
+			klayer = keras.layers.Dense(neurons_units, activation=activations[activation], kernel_initializer='glorot_normal')
+		elif array[0] == 2:
+			klayer = keras.layers.LSTM(neurons_units, activation=activations[activation], kernel_initializer='glorot_normal', return_sequences=return_sequences)
+		elif array[0] == 3:
+			klayer = keras.layers.Conv2D(filter_size, (kernel_size, kernel_size), strides=(stride, stride), padding='valid',
+				activation=activations[activation], kernel_initializer='glorot_normal')
+		elif array[0] == 4:
+			klayer = keras.layers.MaxPooling2D(pool_size=(pool_size, pool_size))
+		elif array[0] == 5:
+			klayer = keras.layers.Dropout(dropout_rate)
+		else:
+			print("Layer not valid")
+			klayer = None
+
+	return klayer
+
+
+
+"""Input can be of 3 types, ANN (1), CNN (2) or RNN (3)"""
 def main():
 
-	architecture_type = 4
+	architecture_type = 1
 	problem_type = 1  #1 for regression, 2 for classification
 	number_classes = 8 #If regression applies, number of classes
 
@@ -140,17 +261,21 @@ def main():
 
 	#Last layer is always FC
 	if problem_type == 1:
-		layer_last = [1, 1, 3, 0, 0, 0]
+		layer_last = [1, 1, 3, 0, 0, 0, 0, 0]
 	else:
-		layer_last = [1, number_classes, 4, 0, 0, 0]
+		layer_last = [1, number_classes, 4, 0, 0, 0, 0, 0]
 
 	#print(model)
 
 	model.append(layer_last)
 	model_full = [layer_first] + model
 
-	#print(model_full)
-	decode_genotype(model_full)
+	print(model_full)
+	model = decode_genotype(model_full, problem_type, (16,), 1)
+
+	if model != None:
+		model.summary()
+
 
 
 

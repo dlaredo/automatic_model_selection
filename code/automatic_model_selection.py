d5d2c1c2c4b2ce5a82e7c164cad57f1049e45ba8
dlaredo <davidlaredo1@gmail.com>
2019-01-18 18:26:17 -0800

    Creating notebook for automatic model selection

--- code/automatic_model_selection.py
+++ code/automatic_model_selection.py
@@ -6,26 +6,10 @@ import logging
 import sys
 import numpy as np
 
-sys.path.append('/Users/davidlaredorazo/Documents/University_of_California/Research/Projects')
-#sys.path.append('/Users/davidlaredorazo/Documents/University_of_California/Research/Projects')
-
-from CMAPSAuxFunctions import TrainValTensorBoard
 from keras import backend as K
 from keras.callbacks import LearningRateScheduler
-from ann_encoding_rules import Layers
+
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-
-#Tunable model
-from ann_framework.tunable_model.tunable_model import SequenceTunableModelRegression
-#from tunable_model import SequenceTunableModelRegression
-
-#Data handlers
-from ann_framework.data_handlers.data_handler_CMAPS import CMAPSDataHandler
-from ann_framework.data_handlers.data_handler_MNIST import MNISTDataHandler
-#from data_handler_MNIST import MNISTDataHandler
-#from data_handler_CMAPS import CMAPSDataHandler
-
-
 
 #from data_handlers import data_handler_MNIST
 
@@ -373,90 +357,11 @@ def run_experiment(configuration, data_handler, experiment_number, unroll=False,
 	return experiment_best
 
 
-def cmaps_dhandler():
-
-	#Selected as per CNN paper
-	features = ['T2', 'T24', 'T30', 'T50', 'P2', 'P15', 'P30', 'Nf', 'Nc', 'epr', 'Ps30', 'phi', 'NRf', 'NRc', 'BPR', 
-	'farB', 'htBleed', 'Nf_dmd', 'PCNfR_dmd', 'W31', 'W32']
-	selected_indices = np.array([2, 3, 4, 7, 8, 9, 11, 12, 13, 14, 15, 17, 20, 21])
-	selected_features = list(features[i] for i in selected_indices-1)
-	data_folder = '../CMAPSSData'
-
-	window_size = 25
-	window_stride = 1
-	max_rul = 130
-
-	dHandler_cmaps = CMAPSDataHandler(data_folder, 1, selected_features, max_rul, window_size, window_stride)
-
-	input_shape = (len(selected_features)*window_size, )
-
-	return dHandler_cmaps, input_shape
-
 def print_best(best_list):
 
 	for i in best_list:
 		print(i)
 		logging.info(i)
-
-
-def main():
-	"""Input can be of 3 types, ANN (1), CNN (2) or RNN (3)"""
-	architecture_type = Layers.FullyConnected
-	problem_type = 1  #1 for regression, 2 for classification
-	output_shape = 1 #If regression applies, number of classes
-	input_shape = (784,)
-	pop_size = 5
-	tournament_size = 3
-	max_similar = 3
-	total_experiments = 5
-	#new_experiment = True
-	count_experiments = 0
-	unroll = True
-
-	global_best_list = []
-	global_best = None
-
-	min_max_scaler = MinMaxScaler(feature_range=(-1, 1))
-
-	t = datetime.datetime.now()
-
-	logging.basicConfig(filename='logs/nn_evolution_' + t.strftime('%m%d%Y%H%M%S') + '.log', level=logging.INFO, 
-		format='%(levelname)s:%(threadName)s:%(message)s', datefmt='%m/%d/%Y %H:%M:%S')
-
-	#cmaps datahandler
-	dhandler_cmaps, input_shape = cmaps_dhandler()
-
-	#mnist datahandler
-	#dHandler_mnist = MNISTDataHandler()
-
-	config = Configuration(architecture_type, problem_type, input_shape, output_shape, pop_size, tournament_size, max_similar, epochs=5, cross_val=0.2, size_scaler=0.4,
-			       max_generations=10, binary_selection=True, mutation_ratio=0.4, similarity_threshold=0.2, more_layers_prob=0.8)
-
-	while count_experiments < total_experiments:
-		print("Launching experiment {}".format(count_experiments+1))
-		logging.info("Launching experiment {}".format(count_experiments+1))
-		best = run_experiment(config, dHandler_mnist, count_experiments + 1, unroll=unroll, verbose_data=0, tModel_scaler=min_max_scaler)
-
-		global_best_list.append(best)
-
-		if global_best == None:
-			global_best = best
-		else:
-			if best.fitness < global_best.fitness:
-				global_best = best
-
-		count_experiments =  count_experiments + 1
-
-	print("Global best list\n")
-	logging.info("Global best list\n")
-	print_best(global_best_list)
-	
-	print("Global best is\n")
-	print(global_best)
-	logging.info("Global best is\n")
-	logging.info(global_best)
-
-main()
 
 
 
